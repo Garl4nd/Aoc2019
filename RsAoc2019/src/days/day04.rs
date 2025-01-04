a0e6c30875865e317d1de80ca76374852ed74ad4
Filip Kostka, W <garland@DESKTOP-6D26JKA>
2025-01-04 19:17:32 +0100

    Learned to simulate recursion with stack

--- RsAoc2019/src/days/day04.rs
+++ RsAoc2019/src/days/day04.rs
@@ -12,51 +12,60 @@ use std::{collections::HashSet, fs::read_to_string};
 //     | currentPlace == fixedPlace = [11 * n + 100 * rest | n <- [1 .. currentMax], rest <- go (currentPlace + 2) n]
 //     | otherwise = [val + 10 * rest | val <- [1 .. currentMax], rest <- go (currentPlace + 1) val]
 //
-fn get_nums1_stack(places: i32, fixed_place: i32) -> Vec<i32> {
-    let mut nums_and_mins: Vec<(i32, i32)> = vec![(0, 1)];
-    // for currentPlace in 1..=places {
-    let mut current_place = 1;
-    let mut max = 9;
-    let mut stack : Vec<(i32, i32, i32, Vec<i32>)> = vec![(1, 1,9, Vec::new())];
-    let mut res : Option(Vec<i32>) = None ;//Vec::new();
-    while !stack.is_empty(){
-        if current_place == places{
-             res = Some((1..max).collect());
-             current_place -= 1;
-        }
-        else 
-    {
-        
-            
-        if let Some(resVec) = res    
-        {
-            let (current_place_, val, max_, mut current_list) = stack.pop().unwrap();   
-                current_list.extend(resVec.iter()); //push(val + 10 *r)
-                res = None ;
-             current_place = current_place_+1; 
-             max = val ; 
-    }
-        {
-            if val <= max_{
-             stack.push((current_place_, val+1, max, current_list));
-           }
-            else {
-                res_stack = current_list;
+fn fib(n: i32) -> i32 {
+    let mut stack: Vec<(i32, i32, i32)> = vec![(n, 0, 1)];
+    let mut res: Option<i32> = None;
+    while let Some((n, mut val, position)) = stack.pop() {
+        // println!("{:?}", (n, val, position));
+        if n < 2 {
+            res = Some(n);
+        } else if position == 4 {
+            res = Some(val);
+        } else {
+            if let Some(res_val) = res {
+                val += res_val;
+                res = None;
             }
-                }
+            // println!("Below: {:?}", (n, val, position));
+            stack.push((n, val, position + 1));
+            stack.push((n - position, 0, 1));
+        }
     }
-    while current_place <= places {
-            nums_and_mins = nums_and_mins
-                .iter()
-                .flat_map(|(num, min)| (*min..=9).map(|num_| (10 * (*num) + num_, num_)))
-                .collect();
-            current_place += 1;
-        };
-    
 
-    nums_and_mins.iter().map(|(n, _)| *n).collect()
+    res.unwrap()
 }
-
+fn get_nums1_stack(places: i32, fixed_place: i32) -> Vec<i32> {
+    // for currentPlace in 1..=places {
+    let mut stack: Vec<(i32, i32, i32, Vec<i32>)> = vec![(1, 1, 9, Vec::new())];
+    let mut res: Option<Vec<i32>> = None; //Vec::new();
+    while let Some((current_place, mut val, max, mut current_list)) = stack.pop() {
+        if current_place == places {
+            res = Some((1..=max).collect());
+        } else {
+            if let Some(res_vec) = res {
+                current_list.extend(res_vec.iter().map(|r| val + 10 * r)); //push(val + 10 *r)
+                res = None;
+                val += 1;
+            }
+            if val <= max {
+                stack.push((current_place, val, max, current_list));
+                stack.push((current_place + 1, 0, val, Vec::new()))
+            } else {
+                res = Some(current_list);
+            }
+        }
+        // while current_place <= places {
+        //         nums_and_mins = nums_and_mins
+        //             .iter()
+        //             .flat_map(|(num, min)| (*min..=9).map(|num_| (10 * (*num) + num_, num_)))
+        //             .collect();
+        //         current_place += 1;
+        //     };
+        //
+        //
+    }
+    res.unwrap()
+}
 fn get_nums1_f(places: i32, fixed_place: i32) -> Vec<i32> {
     let mut nums_and_mins: Vec<(i32, i32)> = vec![(0, 1)];
     // for currentPlace in 1..=places {
@@ -133,7 +142,11 @@ fn get_nums2(places: i32) -> Vec<i32> {
 }
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    println!("{:?}", get_nums1(3));
+    // println!("{:?}", get_nums1(3));
+    println!("{:?}", get_nums1_stack(3, 0));
+    for i in 0..=10 {
+        println!("fib {} = {} ", i, fib(i));
+    }
     let nums = get_nums1(6);
     let nums2 = get_nums2(6);
     let sol1: u64 = nums.len() as u64;
