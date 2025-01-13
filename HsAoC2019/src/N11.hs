1f9245218884b565fee560bc0e8f6e24fb49f93f
root <root@localhost.localdomain>
2025-01-13 22:39:36 +0000

    Solved #11

--- HsAoC2019/src/N11.hs
+++ HsAoC2019/src/N11.hs
@@ -11,19 +11,21 @@ import Data.Array.ST (runSTUArray)
 import qualified Data.Array.Unboxed as A
 import IntCode
 import Useful
+import Data.List (intercalate)
 
 data Direction = U | D | L | R deriving (Show, Eq)
 width, height :: Int
-width = 100
-height = 100
+width =  45
+height = 45
 
-paintShip :: [Int] -> A.UArray GridPos Bool
-paintShip code = runSTUArray calc
+paintShip :: Int -> [Int] -> (A.Array GridPos Int, A.UArray GridPos Bool)
+paintShip initTile code = runST calc
  where
-  calc :: forall s. ST s (STUArray s GridPos Bool)
+  calc :: forall s. ST s (A.Array GridPos Int, A.UArray GridPos Bool)
   calc = do
     shipAr <- newArray @(STUArray s) ((-height, -width), (height, width)) 0
-    paintedPanels <- newArray ((-height, -width), (height, width)) False
+    writeArray shipAr (0,0) initTile 
+    paintedPanels <- newArray @(STUArray s) ((-height, -width), (height, width)) False
     computer <- createMachine @(STUArray s) code
     flip fix ((0, 0), U) $ \loop (currentPos, currentDir) -> do
       currentColor <- readArray shipAr currentPos
@@ -35,8 +37,21 @@ paintShip code = runSTUArray calc
         let newDir = newDirection turnCode currentDir
         let newPos = turn currentPos newDir
         loop (newPos, newDir)
+    shipArFr <- freeze shipAr 
+    paintedPanelsFr <- freeze paintedPanels 
+    return (shipArFr, paintedPanelsFr)
 
-    return paintedPanels
+solution1 :: [Int] -> Int 
+solution1 code = let 
+  (_, paintedPanels) = paintShip 0 code 
+  in count True $ A.elems paintedPanels
+
+solution2 :: [Int] -> String 
+solution2 code = let 
+  (finalPaintEncoded, _) = paintShip 1 code 
+  finalPaint = (\x -> if x>0 then 'O' else ' ') <$> finalPaintEncoded
+  in intercalate "\n" $  charGridToStr finalPaint
+
 
 newDirection :: Int -> Direction -> Direction
 newDirection 0 = \case
