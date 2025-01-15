e760bf10534310a8d3dc06fefc0f3831f5fa36d8
root <root@localhost.localdomain>
2025-01-15 23:19:59 +0000

    Played the game to find out the solution to #13

--- HsAoC2019/src/N13.hs
+++ HsAoC2019/src/N13.hs
@@ -26,7 +26,7 @@ objIdToChar = \case
   0 -> ' '
   1 -> 'x'
   2 -> '!'
-  3 -> '_'
+  3 -> 'T'
   4 -> 'O'
   _ -> error "Wrong id"
 
@@ -35,24 +35,32 @@ updateGameAr updates grid =
   grid // [((y, x), objIdToChar objId) | [x, y, objId] <- updates]
 getSolutions13 = getSolutions codeParser solution1 (const 0)
 dim :: Int
-dim = 50
+dim = 45
+autoMode = True 
 playGame :: [Int] -> IO ()
 playGame code = do
   machine <- createMachine @IOArray (2 : tail code)
   let gameAr = A.listArray ((0, 0), (dim, dim)) [' ' | _ <- [0 .. dim], _ <- [0 .. dim]]
-  flip fix gameAr $ \loop currentAr -> do
-    input <- subtract 107 . ord <$> getChar
-    when (input == -3) $ playGame code
+  
+  flip fix (gameAr, 0, 0) $ \loop (currentAr, autoInput, score) -> do
+    userInput <-  getChar
+    let input = if userInput == 'a' then autoInput else subtract 107 . ord $ userInput 
+    when (input == -4) $ playGame code
     when (input < -1 || input > 1) $ do
-      putStrLn "Invalid input!"
-      loop currentAr
+      -- putStrLn "Invalid input!"
+      loop (currentAr, 0, score)
     -- print input
+    
     outputs <- getOutputs =<< runMachine [input] machine
+    when (null outputs) $ loop (currentAr, 0, score)
     -- print outputs
     let (resList, updates) = partition (\[x, y, _] -> x == -1 && y == 0) $ chunksOf 3 outputs
-    case resList of
-      [] -> return ()
-      res : _ -> print $ "score = " <> let [_, _, score] = res in show score
+    let platformPos = case [x | [x,_,3] <- updates] of [] -> 0; f:_ -> f
+        ballPos = case  [x | [x,_,4] <- updates] of [] -> 0; f:_ -> f
+        updatedScore =  case resList of
+            [] -> score 
+            res : _ -> let [_, _, newScore] = res in newScore  
+    putStrLn $ "score = " <> show updatedScore
     let newAr = updateGameAr updates currentAr
     putStrLn $ unlines . charGridToStr $ newAr
-    loop newAr
+    loop (newAr, if ballPos > platformPos then 1 else -1, updatedScore)
