e75c62d533eab587b3e7ea1bbf7f50893ede1003
root <root@PC-LIM-407.firma.lim.cz>
2025-01-15 18:03:46 +0100

    Implement intcode game

--- HsAoC2019/src/N13.hs
+++ HsAoC2019/src/N13.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
+
 module N13 () where
 
+import Control.Monad (void, when)
+import Data.Array ((!), (//))
+import qualified Data.Array as A
+import Data.Array.IO (IOArray)
+import Data.Char (ord)
+import Data.Function (fix)
+import Data.List (partition)
 import IntCode
 import Useful
 
@@ -12,5 +21,38 @@ solution1 code =
    in
     length tilePos
 
--- renderGame  :: [Int] -> [String]
+objIdToChar :: Int -> Char
+objIdToChar = \case
+  0 -> ' '
+  1 -> 'x'
+  2 -> '!'
+  3 -> '_'
+  4 -> 'O'
+  _ -> error "Wrong id"
+
+updateGameAr :: [[Int]] -> CharGrid -> CharGrid
+updateGameAr updates grid =
+  grid // [((y, x), objIdToChar objId) | [x, y, objId] <- updates]
 getSolutions13 = getSolutions codeParser solution1 (const 0)
+dim :: Int
+dim = 50
+playGame :: [Int] -> IO ()
+playGame code = do
+  machine <- createMachine @IOArray (2 : tail code)
+  let gameAr = A.listArray ((0, 0), (dim, dim)) [' ' | _ <- [0 .. dim], _ <- [0 .. dim]]
+  flip fix gameAr $ \loop currentAr -> do
+    input <- subtract 107 . ord <$> getChar
+    when (input == -3) $ playGame code
+    when (input < -1 || input > 1) $ do
+      putStrLn "Invalid input!"
+      loop currentAr
+    -- print input
+    outputs <- getOutputs =<< runMachine [input] machine
+    -- print outputs
+    let (resList, updates) = partition (\[x, y, _] -> x == -1 && y == 0) $ chunksOf 3 outputs
+    case resList of
+      [] -> return ()
+      res : _ -> print $ "score = " <> let [_, _, score] = res in show score
+    let newAr = updateGameAr updates currentAr
+    putStrLn $ unlines . charGridToStr $ newAr
+    loop newAr
