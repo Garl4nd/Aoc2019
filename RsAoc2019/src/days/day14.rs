aff5180baa3c9880aadc7a10a8a3899b676720b3
root <root@localhost.localdomain>
2025-01-18 23:21:00 +0000

    Refactored #14

--- RsAoc2019/src/days/day14.rs
+++ RsAoc2019/src/days/day14.rs
@@ -9,9 +9,9 @@ struct Reaction<'a> {
     reqs: Vec<Resource<'a>>,
 }
 fn parse_resource(string: &str) -> Resource {
-    let mut strIt = string.trim().split(' ');
-    let amount = strIt.next().unwrap().trim().parse().unwrap();
-    let name = strIt.next().unwrap().trim();
+    let mut str_it = string.trim().split(' ');
+    let amount = str_it.next().unwrap().trim().parse().unwrap();
+    let name = str_it.next().unwrap().trim();
     (name, amount)
 }
 fn parse_file(file: &str) -> HashMap<&str, Reaction> {
@@ -32,8 +32,8 @@ fn parse_file(file: &str) -> HashMap<&str, Reaction> {
         })
         .collect()
 }
+
 fn binary_search(f : impl Fn(i32) -> bool, x0: i32, x1 : i32) -> i32 {
-    // dbg!(x0, x1);
     if x0 +1 == x1 || f(x0)
         {x0}
     else {
@@ -46,39 +46,25 @@ fn binary_search(f : impl Fn(i32) -> bool, x0: i32, x1 : i32) -> i32 {
             binary_search(f, m, x1)}
     }
 }
-fn reaction_reps_and_leftover(
-    target: &str,
+fn consume_supplies_and_find_required_amount(
+    target: &str, 
     requested_amount: i64,
-    prod_count: i64,
-    available_resources: &HashMap<&str, i64>,
-) -> (i64, i64, i64) {
-    // println!("Reps and leftovers:");
-    let available_amount = available_resources.get(target).unwrap();
-    // dbg!(target, requested_amount, prod_count, available_amount);
-    let consumed_amount = std::cmp::min(*available_amount, requested_amount); 
-    let newly_required_amount = requested_amount - consumed_amount; 
-    let (div, rem) = (newly_required_amount / prod_count, newly_required_amount % prod_count);
-        if rem == 0 {
-            // probably could be simplified with negative division and rem. E.g., 15, 8 ->
-            // -15 `quotrem` 8 = (-2, 1) -> (2,1)
-            //
-            (div, prod_count * div, -consumed_amount)
-        } else {
-            (div + 1, prod_count * (div+1),  -consumed_amount)
-        }
-    
+    available_resources: &mut HashMap<&str, i64>
+) -> i64
+{
+    let available_amount = available_resources[target];
+    let consumed_amount = std::cmp::min(available_amount, requested_amount); 
+    *available_resources.get_mut(target).unwrap() -= consumed_amount ; 
+    requested_amount - consumed_amount 
 }
-
 fn minimum_req<'a>(
     reaction_map: &HashMap<&str, Reaction<'a>>,
     target: &str,
     requested_amount: i64,
     available_resources: &mut HashMap<&'a str, i64>,
-    produced_resoucers: &mut HashMap<&'a str, i64>,
+    produced_resoucers: &mut HashMap<& str, i64>,
 ) {
-    // dbg!("Calculating reqs for ", target);
     if target == "ORE" {
-        // println!("Creating {} ore", requested_amount);
         available_resources
             .entry("ORE")
             .and_modify(|o| *o += requested_amount)
@@ -88,41 +74,22 @@ fn minimum_req<'a>(
             .and_modify(|o| *o += requested_amount)
             .or_insert(0);
     } else {
-        let Reaction { prod_count, reqs } = reaction_map.get(target).unwrap();
-        let (reps, newly_produced_amount, leftover) =
-            reaction_reps_and_leftover(target, requested_amount, *prod_count, available_resources);
-        *available_resources.get_mut(target).unwrap() +=  leftover;
-        // dbg!(reps, newly_produced_amount, leftover);
-        for (reactant, reactant_amount) in reqs {
-            let available_amount = available_resources.get(reactant).unwrap();
-            // a tu samou logiku pro ORE, takze z toho spis udelat fukci
-            let required_amount = reactant_amount * reps;   
-            let consumed_amount = std::cmp::min(*available_amount, required_amount); 
-
-            *available_resources.get_mut(reactant).unwrap() -= consumed_amount ; 
-            let newly_produced_amount = required_amount - consumed_amount; 
-            // dbg!(&reactant, required_amount, consumed_amount, newly_produced_amount);
+        let Reaction { prod_count, reqs } = &reaction_map[target];
+        let new_product = consume_supplies_and_find_required_amount(target, requested_amount, available_resources);
+        let reps = (new_product +1) / prod_count;
+       for (reactant, reactant_amount) in reqs {
+            let new_reactant = consume_supplies_and_find_required_amount(reactant, reps*reactant_amount, available_resources);
            minimum_req(
                 reaction_map,
                 reactant,
-                newly_produced_amount,
+                new_reactant,
                 available_resources,
                 produced_resoucers,
             );            
-            *available_resources.get_mut(reactant).unwrap() -= newly_produced_amount ; 
- //            produced_resoucers
- //                .entry(reactant)
- //                .and_modify(|a| {
- //                    *a += newly_produced_amount;
- //                })
- //                .or_insert(0);
- // 
-               }
-        *available_resources.get_mut(target).unwrap() += newly_produced_amount;
-        *produced_resoucers.get_mut(target).unwrap() +=  newly_produced_amount ;
-        // dbg!(&available_resources);
-        // dbg!(&produced_resoucers);
-
+            *available_resources.get_mut(reactant).unwrap() -= new_reactant ; 
+              }
+        *available_resources.get_mut(target).unwrap() += new_product;
+        *produced_resoucers.get_mut(target).unwrap() +=  new_product ;
     };
 }
 fn required_ore(reaction_map: &HashMap<&str, Reaction>, fuel: i64) -> i64{
@@ -154,6 +121,5 @@ pub fn solve() -> SolutionPair {
     let reaction_map  = parse_file(&file);
     let sol1: u64 = solution1(&reaction_map);
     let sol2: u64 = solution2(&reaction_map);
-
     (Solution::from(sol1), Solution::from(sol2))
 }
