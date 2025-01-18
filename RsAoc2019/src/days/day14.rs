67a5ff1237f552f210eb569c6b87667c43aa2cc3
root <root@localhost.localdomain>
2025-01-18 15:03:44 +0000

    Solved #14a

--- RsAoc2019/src/days/day14.rs
+++ RsAoc2019/src/days/day14.rs
@@ -23,7 +23,7 @@ fn parse_file(file: &str) -> HashMap<&str, Reaction> {
         .map(|(resources, product)| {
             let (name, prod_count) = parse_resource(product);
             (
-                name.clone(),
+                name,
                 Reaction {
                     prod_count,
                     reqs: resources.split(',').map(parse_resource).collect(),
@@ -44,21 +44,22 @@ fn reaction_reps_and_leftover(
     requested_amount: i32,
     prod_count: i32,
     available_resources: &HashMap<&str, i32>,
-) -> (i32, i32) {
-    let required_amount = requested_amount - available_resources.get(target).unwrap();
-    if required_amount <= 0 {
-        (0, -required_amount)
-    } else {
-        let (div, rem) = (required_amount / prod_count, required_amount % prod_count);
+) -> (i32, i32, i32) {
+    println!("Reps and leftovers:");
+    let available_amount = available_resources.get(target).unwrap();
+    dbg!(target, requested_amount, prod_count, available_amount);
+    let consumed_amount = std::cmp::min(*available_amount, requested_amount); 
+    let newly_required_amount = requested_amount - consumed_amount; 
+    let (div, rem) = (newly_required_amount / prod_count, newly_required_amount % prod_count);
         if rem == 0 {
             // probably could be simplified with negative division and rem. E.g., 15, 8 ->
             // -15 `quotrem` 8 = (-2, 1) -> (2,1)
             //
-            (div, 0)
+            (div, prod_count * div, -consumed_amount)
         } else {
-            (div + 1, prod_count - rem)
+            (div + 1, prod_count * (div+1),  -consumed_amount)
         }
-    }
+    
 }
 
 fn minimum_req<'a>(
@@ -68,7 +69,9 @@ fn minimum_req<'a>(
     available_resources: &mut HashMap<&'a str, i32>,
     produced_resoucers: &mut HashMap<&'a str, i32>,
 ) {
+    dbg!("Calculating reqs for ", target);
     if target == "ORE" {
+        println!("Creating {} ore", requested_amount);
         available_resources
             .entry("ORE")
             .and_modify(|o| *o += requested_amount)
@@ -79,30 +82,40 @@ fn minimum_req<'a>(
             .or_insert(0);
     } else {
         let Reaction { prod_count, reqs } = reaction_map.get(target).unwrap();
-        let (reps, leftover) =
+        let (reps, newly_produced_amount, leftover) =
             reaction_reps_and_leftover(target, requested_amount, *prod_count, available_resources);
+        *available_resources.get_mut(target).unwrap() +=  leftover;
+        dbg!(reps, newly_produced_amount, leftover);
         for (reactant, reactant_amount) in reqs {
-            dbg!(&reactant);
-            let available_amount = available_resources.get_mut(reactant).unwrap();
-            let required_amount = std::cmp::max(0, reactant_amount * reps - *available_amount);
-            *available_amount -= required_amount;
-            produced_resoucers
-                .entry(reactant)
-                .and_modify(|a| {
-                    *a += required_amount;
-                })
-                .or_insert(0);
-            minimum_req(
+            let available_amount = available_resources.get(reactant).unwrap();
+            // a tu samou logiku pro ORE, takze z toho spis udelat fukci
+            let required_amount = reactant_amount * reps;   
+            let consumed_amount = std::cmp::min(*available_amount, required_amount); 
+
+            *available_resources.get_mut(reactant).unwrap() -= consumed_amount ; 
+            let newly_produced_amount = required_amount - consumed_amount; 
+            dbg!(&reactant, required_amount, consumed_amount, newly_produced_amount);
+           minimum_req(
                 reaction_map,
                 reactant,
-                required_amount,
+                newly_produced_amount,
                 available_resources,
                 produced_resoucers,
-            );
-            dbg!(&produced_resoucers);
-        }
-        *available_resources.get_mut(target).unwrap() += leftover;
-        *produced_resoucers.get_mut(target).unwrap() += requested_amount + leftover;
+            );            
+            *available_resources.get_mut(reactant).unwrap() -= newly_produced_amount ; 
+ //            produced_resoucers
+ //                .entry(reactant)
+ //                .and_modify(|a| {
+ //                    *a += newly_produced_amount;
+ //                })
+ //                .or_insert(0);
+ // 
+               }
+        *available_resources.get_mut(target).unwrap() += newly_produced_amount;
+        *produced_resoucers.get_mut(target).unwrap() +=  newly_produced_amount ;
+        dbg!(&available_resources);
+        dbg!(&produced_resoucers);
+
     };
 }
 pub fn solve() -> SolutionPair {
@@ -121,7 +134,6 @@ pub fn solve() -> SolutionPair {
         &mut available_resoures,
         &mut produced_resources,
     );
-    dbg!(reacts);
     dbg!(&produced_resources);
     dbg!(produced_resources.get("ORE").unwrap());
     let sol1: u64 = 0;
