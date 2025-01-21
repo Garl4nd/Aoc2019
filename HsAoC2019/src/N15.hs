beeb95af1f1dfdc3aaeb621b39e1739ee107fb24
root <root@PC-LIM-407.firma.lim.cz>
2025-01-21 11:00:11 +0100

    Made it possible to explore with the robot manually in #15, used this to solve part 1

--- HsAoC2019/src/N15.hs
+++ HsAoC2019/src/N15.hs
@@ -25,6 +25,11 @@ directionToInput = \case
   L -> 3
   R -> 4
 
+data Distance = Dist Int | Inf deriving (Show, Eq)
+addDist :: Distance -> Int -> Distance
+addDist (Dist c) d = Dist (c + d)
+addDist _ _ = Inf
+
 data Direction = U | D | L | R deriving (Show)
 turn :: GridPos -> Direction -> GridPos
 turn (y, x) U = (y - 1, x)
@@ -38,28 +43,32 @@ outputToChar 1 = '.'
 outputToChar 2 = '!'
 
 bounds :: Int
-bounds = 35  
+bounds = 35
 explore :: [Int] -> IO ()
-explore code = do 
-  robot <- createMachine @IOArray code 
-  let 
-   go pos mazeMap = do
-    clearScreen
-    print pos 
-    putStrLn $ unlines . charGridToStr $ mazeMap
-    directionChar <- getChar
-    let direction = charToDirection directionChar
-    case direction of
-      Just dir -> do
-        let input = directionToInput dir
-            desiredPosition = turn pos dir
-        outputs <- getOutputs =<< runMachine [input] robot
-        when (null outputs) $ go pos mazeMap
-        let outputCode = head outputs
-            updatedMap = mazeMap // [(desiredPosition, outputToChar outputCode)]
-            updatedPos = if outputCode == 0 then pos else desiredPosition
-        go updatedPos updatedMap
-      Nothing -> go pos mazeMap 
-  go (0, 0) $ A.listArray ((-bounds, -bounds), (bounds, bounds)) ['?' | _ <- [-bounds .. bounds], _ <- [-bounds .. bounds]]
- 
-
+explore code = do
+  robot <- createMachine @IOArray code
+  let
+    mazeMap = A.listArray ((-bounds, -bounds), (bounds, bounds)) [' ' | _ <- [-bounds .. bounds], _ <- [-bounds .. bounds]]
+    distMap = (Inf <$ mazeMap) // [((0, 0), Dist 0)]
+    go pos mazeMap currentDist distMap = do
+      clearScreen
+      print (pos, currentDist)
+      putStrLn $ unlines . charGridToStr $ mazeMap // [(pos, 'R')]
+      directionChar <- getChar
+      let direction = charToDirection directionChar
+      case direction of
+        Just dir -> do
+          let input = directionToInput dir
+              desiredPosition = turn pos dir
+          outputs <- getOutputs =<< runMachine [input] robot
+          when (null outputs) $ go pos mazeMap currentDist distMap
+          let outputCode = head outputs
+              updatedMap = mazeMap // [(desiredPosition, outputToChar outputCode)]
+              (updatedPos, updatedDist, updatedDistMap) =
+                if
+                  | outputCode == 0 -> (pos, currentDist, distMap)
+                  | distMap ! desiredPosition == Inf -> (desiredPosition, currentDist `addDist` 1, distMap // [(desiredPosition, currentDist `addDist` 1)])
+                  | otherwise -> (desiredPosition, distMap ! desiredPosition, distMap)
+          go updatedPos updatedMap updatedDist updatedDistMap
+        Nothing -> go pos mazeMap currentDist distMap
+  go (0, 0) mazeMap (Dist 0) distMap
