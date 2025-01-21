4308a3661f1dda68f5e89d06c289dec464888929
Filip Kostka, W <garland@DESKTOP-6D26JKA>
2025-01-21 20:06:45 +0100

    Almost solved #15

--- HsAoC2019/src/N15.hs
+++ HsAoC2019/src/N15.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeApplications #-}
 
 module N15 () where
 
 import Control.Monad (filterM, foldM, when)
+import Control.Monad.Ref (MonadRef (readRef, writeRef), newRef)
 import Data.Array ((!), (//))
 import qualified Data.Array as A
 import Data.Array.IO (IOArray)
 import Data.Array.MArray (freeze, newArray, readArray, writeArray)
 import Data.Foldable (forM_)
+import Data.Functor (void)
 import IntCode
 import System.Console.ANSI
 import Useful
@@ -26,6 +29,11 @@ directionToInput = \case
   D -> 2
   L -> 3
   R -> 4
+opposite :: Direction -> Direction
+opposite D = U
+opposite U = D
+opposite L = R
+opposite R = L
 
 data Distance = Dist Int | Inf | Unknown deriving (Show, Eq)
 addDist :: Distance -> Int -> Distance
@@ -80,45 +88,45 @@ explore code = do
 autoExplore :: [Int] -> IO (A.Array GridPos Distance, Maybe GridPos)
 autoExplore code = do
   robot <- createMachine @IOArray code
-  distMap <- newArray @IOArray ((-bounds, -bounds), (bounds, bounds)) Inf
+  distMap <- newArray @IOArray ((-bounds, -bounds), (bounds, bounds)) Unknown
+  writeArray distMap (0, 0) (Dist 0)
+  oxygenPosRef <- newRef Nothing
   let
-    updateDistMapAndGetWalkablePlaces currentPos currentDist = do
-      unvisitedDirections <- filterM @IO (\dir -> readArray distMap (turn currentPos dir) >>= \dist -> return $ dist == Unknown) [U, L, D, R]
-      let unvisitedPositions = turn currentPos <$> unvisitedDirections
-      outputs <- sequence [fmap head . getOutputs =<< runMachine [directionToInput dir] robot | dir <- unvisitedDirections]
+    updateDistMapAndGetWalkables :: GridPos -> Distance -> IO [(GridPos, Direction)]
+    updateDistMapAndGetWalkables currentPos currentDist =
       foldM
-        ( \walkablePlaces (o, pos) -> do
-            if o == 0
-              then do
-                writeArray distMap pos Inf
-                return walkablePlaces
-              else do
-                writeArray distMap pos $ addDist currentDist 1
-                return $ pos : walkablePlaces
+        ( \walkableDirections dir -> do
+            let pos = turn currentPos dir
+            valAtPos <- readArray distMap pos
+            -- print (pos, valAtPos)
+            case valAtPos of
+              Unknown -> do
+                output <- fmap head . getOutputs =<< runMachine [directionToInput dir] robot
+                if output == 0
+                  then do
+                    writeArray distMap pos Inf
+                    return walkableDirections
+                  else do
+                    _ <- runMachine [directionToInput $ opposite dir] robot
+                    when (output == 2) $ writeRef oxygenPosRef $ Just pos
+                    writeArray distMap pos $ addDist currentDist 1
+                    return $ (pos, dir) : walkableDirections
+              _ -> return walkableDirections
         )
         []
-        $ zip outputs unvisitedPositions
+        [U, D, L, R]
+    walk :: Direction -> IO ()
+    walk dir = void $ runMachine [directionToInput dir] robot
+    goBack = mapM_ (walk . opposite)
+    go pos currentDist initialPos breadcrumbs = do
+      walkables <- updateDistMapAndGetWalkables pos currentDist
+      print walkables
+      case walkables of
+        [] -> goBack breadcrumbs
+        [(newPos, newDir)] -> walk newDir >> go newPos (addDist currentDist 1) initialPos (newDir : breadcrumbs)
+        _ -> sequence_ [walk newDir >> go newPos (addDist currentDist 1) pos [newDir] | (newPos, newDir) <- walkables]
 
-    go pos initialPos currentDist = do
-      walkablePlaces <- updateDistMapAndGetWalkablePlaces pos currentDist
-      let goBack = const $ return ()
-      case walkablePlaces of
-        [] -> goBack initialPos
-        [newPos] -> go newPos initialPos currentDist
-        _ -> sequence_ [go newPos pos currentDist | newPos <- walkablePlaces]
-        Just dir -> do
-          let input = directionToInput dir
-              desiredPosition = turn pos dir
-          outputs <- getOutputs =<< runMachine [input] robot
-          -- when (null outputs) $ go pos mazeMap currentDist distMap
-          let outputCode = head outputs
-              updatedMap = mazeMap // [(desiredPosition, outputToChar outputCode)]
-              (updatedPos, updatedDist, updatedDistMap) =
-                if
-                  | outputCode == 0 -> (pos, currentDist, distMap)
-                  | distMap ! desiredPosition == Inf -> (desiredPosition, currentDist `addDist` 1, distMap // [(desiredPosition, currentDist `addDist` 1)])
-                  | otherwise -> (desiredPosition, distMap ! desiredPosition, distMap)
-          go updatedPos updatedDist updatedDistMap
-        Nothing -> go pos currentDist distMap
-
-  return (freeze distMap, Just (0, 0))
+  go (0, 0) (Dist 0) (0, 0) []
+  ar <- freeze distMap
+  oxygenPos <- readRef oxygenPosRef
+  return (ar, oxygenPos)
