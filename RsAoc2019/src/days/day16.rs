57e1171f1eff25f2a66ea16e8f3f20918b417b39
root <root@PC-LIM-407.firma.lim.cz>
2025-01-23 16:01:17 +0100

    Started solving #16, built a matrix for the problem using nalgebra

--- RsAoc2019/src/days/day16.rs
+++ RsAoc2019/src/days/day16.rs
@@ -1,12 +1,32 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
-
+use itertools::Itertools;
+use nalgebra::{
+    self, ArrayStorage, Dyn, Matrix, OMatrix, RowOVector, RowVector, VecStorage, U2, U3,
+};
+use std::{
+    fs::read_to_string,
+    iter::{repeat, repeat_n},
+};
 ///////////////////////////////////////////////////////////////////////////////
+type DMatrix<T> = OMatrix<T, Dyn, Dyn>;
+fn buildMatrix(n: usize) -> DMatrix<i32> {
+    let rows: Vec<RowOVector<i32, Dyn>> = Vec::new();
+    let base_pattern = [0, 1, 0, -1];
+    let row_it = (0..n).into_iter().flat_map(|i| {
+        let basic_pattern = base_pattern.iter().flat_map(move |el| repeat_n(*el, i + 1));
+        repeat(basic_pattern).flatten().take(n + 1).dropping(1)
+    });
+    DMatrix::<i32>::from_row_iterator(n, n, row_it)
+    //    Matrix
+    //  DMatrix::from_rows(&rows)
+}
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
+    let matrix = buildMatrix(12);
+    print!("{}", matrix);
     (Solution::from(sol1), Solution::from(sol2))
 }
