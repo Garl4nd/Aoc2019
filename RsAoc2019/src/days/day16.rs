b16a68994afe4ab08c668e5d211bb96d3421a85c
root <root@PC-LIM-407.firma.lim.cz>
2025-01-23 17:32:20 +0100

    Solved #16a

--- RsAoc2019/src/days/day16.rs
+++ RsAoc2019/src/days/day16.rs
@@ -1,7 +1,8 @@
 use crate::{Solution, SolutionPair};
-use itertools::Itertools;
+use itertools::{iterate, Itertools};
 use nalgebra::{
-    self, ArrayStorage, Dyn, Matrix, OMatrix, RowOVector, RowVector, VecStorage, U2, U3,
+    self, ArrayStorage, DVector, Dyn, Matrix, OMatrix, OVector, RowOVector, RowVector, VecStorage,
+    U2, U3,
 };
 use std::{
     fs::read_to_string,
@@ -9,10 +10,9 @@ use std::{
 };
 ///////////////////////////////////////////////////////////////////////////////
 type DMatrix<T> = OMatrix<T, Dyn, Dyn>;
-fn buildMatrix(n: usize) -> DMatrix<i32> {
-    let rows: Vec<RowOVector<i32, Dyn>> = Vec::new();
+fn build_matrix(n: usize) -> DMatrix<i32> {
     let base_pattern = [0, 1, 0, -1];
-    let row_it = (0..n).into_iter().flat_map(|i| {
+    let row_it = (0..n).flat_map(|i| {
         let basic_pattern = base_pattern.iter().flat_map(move |el| repeat_n(*el, i + 1));
         repeat(basic_pattern).flatten().take(n + 1).dropping(1)
     });
@@ -20,13 +20,35 @@ fn buildMatrix(n: usize) -> DMatrix<i32> {
     //    Matrix
     //  DMatrix::from_rows(&rows)
 }
-
+fn parse_file(file: &str) -> DVector<i32> {
+    let nums = file
+        .lines()
+        .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as i32))
+        .collect();
+    DVector::from_vec(nums)
+}
+fn multiPhaseCalc(phases: usize, mat: &DMatrix<i32>, vec: &DVector<i32>) -> DVector<i32> {
+    iterate(vec.clone(), |vec| {
+        let mul = mat * (vec);
+        mul.map(|r| if r > 0 { r % 10 } else { (-r) % 10 })
+    })
+    .nth(phases)
+    .unwrap()
+}
 pub fn solve() -> SolutionPair {
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
-
-    let matrix = buildMatrix(12);
+    let file = read_to_string("input/16_test.txt").unwrap();
+    dbg!(&file);
+    let nums = parse_file(&file);
+    let matrix = build_matrix(nums.len());
+    print!("vec= {}", nums);
     print!("{}", matrix);
+    for phase in 0..10 {
+        let res = multiPhaseCalc(phase, &matrix, &nums);
+
+        println!("res = {}", res);
+    }
     (Solution::from(sol1), Solution::from(sol2))
 }
