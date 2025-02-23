2feae0d9f95eb9a9dd79996be0209b56be9411bc
root <root@localhost.localdomain>
2025-02-23 23:17:56 +0000

    Improved performance by turning the explorers into a stack, formalized the solution

--- RsAoc2019/src/days/day18.rs
+++ RsAoc2019/src/days/day18.rs
@@ -13,32 +13,6 @@ use std::{
     time::Duration,
 };
 
-// trait Encode {
-//     fn empty() -> Self;
-//     fn encode(symbol: char) -> Self
-//     where
-//         Self: Sized,
-//     {
-//         Self::empty().add(symbol)
-//     }
-//     fn get_encoding(&self) -> i32;
-//     fn from_encoding(enc: i32) -> Self;
-//     fn add(&self, new_symbol: char) -> Self
-//     where
-//         Self: Sized,
-//     {
-//         let code = self.get_encoding();
-//         let new_code = Self::encode(new_symbol).get_encoding();
-//         Self::from_encoding(code | new_code)
-//     }
-// }
-// enum EncodingType {
-//     Door,
-//     Key,
-// }
-// struct Keys(i32);
-// struct Doors(i32);
-// impl Encode for Keys {
 #[derive(Debug)]
 struct Keys(i32);
 #[derive(Debug, Clone)]
@@ -96,61 +70,51 @@ struct KeyPath {
     path_length: usize,
     door_list: Doors,
 }
-#[derive(Debug)]
-struct Explorer {
-    pos: GridPos,
-    door_list: Doors,
-}
 ///////////////////////////////////////////////////////////////////////////////
 fn key_paths(maze: &GridVec<char>, init_pos: GridPos, init_key: char) -> Vec<KeyPath> {
-    let mut explorers = vec![Explorer {
+    
+ struct Explorer {
+    pos: GridPos,
+    door_list: Doors,
+    dist: usize
+}
+   let mut explorers = vec![Explorer {
         pos: init_pos,
         door_list: Doors::empty(),
+       dist: 0
     }];
     let mut visited_pos = HashSet::<GridPos>::new();
     // let mut found_keys = HashSet::<char>::new();
     let mut found_paths = Vec::<KeyPath>::new();
-    let mut distance: usize = 0;
+    // let mut distance: usize = 0;
 
-    while !explorers.is_empty() {
-        let mut new_explorers = Vec::<Explorer>::new();
-        for explorer in &mut explorers {
+    while let Some(mut explorer) = explorers.pop(){ 
             visited_pos.insert(explorer.pos);
             let symbol = maze[explorer.pos];
             if symbol.is_ascii_lowercase() && symbol != init_key {
-                //&& !found_keys.contains(&symbol) {
 
                 found_paths.push(KeyPath {
                     key: symbol,
-                    path_length: distance,
+                    path_length: explorer.dist,
                     door_list: explorer.door_list.clone(),
                 });
-                // found_keys.insert(symbol);
             }
             if symbol.is_ascii_uppercase() {
-                // dbg!("Found door", symbol, &explorer.door_list);
                 explorer.door_list = explorer.door_list.add(symbol);
-                // dbg!("New door",&explorer.door_list);
             }
             let neighbors = maze
                 .neighbors4(explorer.pos)
                 .into_iter()
                 .filter(|nei| maze[*nei] != '#' && !visited_pos.contains(nei));
-            //dbg!(explorer.pos);
             for neighbor in neighbors {
-                //      dbg!(neighbor);
-                new_explorers.push(Explorer {
+                explorers.push(Explorer {
                     pos: neighbor,
                     door_list: explorer.door_list.clone(),
+                    dist: explorer.dist +1
                 });
-            }
+            
         }
-
-        //dbg!(&explorers, &new_explorers);
-        //sleep(Duration::from_millis(500)
-        //);
-        explorers = new_explorers;
-        distance += 1;
+        // distance += 1;
     }
     found_paths
 }
@@ -169,7 +133,6 @@ fn shortest_path(maze: &GridVec<char>) -> usize {
     let all_key_chars = keypath_map.keys().filter(|c| **c != '@');
     let all_keys = all_key_chars.fold(Keys::empty(), |keys, c| keys.add(*c));
     let openable_doors = Doors(all_keys.0);
-    dbg!(&keypath_map);
     struct Context<'a>(&'a HashMap<char, Vec<KeyPath>>, &'a Keys, &'a Doors);
     let ctx = Context(&keypath_map, &all_keys, &openable_doors);
     fn find_shortest(
@@ -192,10 +155,8 @@ fn shortest_path(maze: &GridVec<char>) -> usize {
                     .filter(|key_path| {
                         !keys.contains(key_path.key)
                             && keys.can_open(&key_path.door_list.intersection(openable_doors))
-                    })
-                    .collect_vec();
+                    });
                 walkable_paths
-                    .into_iter()
                     .map(|key_path| {
                         key_path.path_length
                             + find_shortest(
@@ -203,7 +164,6 @@ fn shortest_path(maze: &GridVec<char>) -> usize {
                                 ctx,
                                 key_path.key,
                                 keys.add(key_path.key),
-                                //                 concat([keys_dbg.clone(), vec![key_path.key]]),
                             )
                     })
                     .min()
@@ -217,22 +177,19 @@ fn shortest_path(maze: &GridVec<char>) -> usize {
     find_shortest(&mut HashMap::new(), &ctx, '@', Keys::empty())
 }
 fn split_maze(maze: &GridVec<char>) -> Vec<GridVec<char>> {
-    let (halfHeight, halfWidth): (i64, i64) =
+    let (half_height, half_width): (i64, i64) =
         ((maze.height() / 2) as i64, (maze.width() / 2) as i64);
     let mut mod_pos = Vec::<(i64, i64)>::new();
     for dy in [-1, 1] {
         for dx in [-1, 1] {
-            mod_pos.push((halfHeight + dy, halfWidth + dx));
+            mod_pos.push((half_height + dy, half_width + dx));
         }
     }
     let mut res = Vec::new();
-    for y_offset in [0, halfHeight + 1] {
-        for x_offset in [0, halfWidth + 1] {
-            let ylb = y_offset as i64;
-            let xlb = x_offset as i64;
-            let yub = (y_offset + halfHeight) as i64 - 1;
-            let xub = (x_offset + halfWidth) as i64 - 1;
-            dbg!(halfHeight, y_offset, x_offset, ylb, xlb, yub, xub);
+    for y_offset in [0, half_height + 1] {
+        for x_offset in [0, half_width + 1] {
+            let (ylb, xlb) = (y_offset, x_offset);
+            let (yub, xub) = (y_offset + half_height - 1,x_offset + half_width - 1);
             let mut new_grid = GridVec::new(' ', ylb, xlb, yub, xub);
             for y in ylb..=yub {
                 for x in xlb..=xub {
@@ -248,39 +205,36 @@ fn split_maze(maze: &GridVec<char>) -> Vec<GridVec<char>> {
     }
     res
 }
+fn solution1(maze: &GridVec<char>) -> usize {
+    shortest_path(maze)
+}
+fn solution2(maze: &GridVec<char>) -> usize {
+    
+    let submazes = split_maze(maze);
+    submazes.iter().map(shortest_path).sum()
+}
+
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
-    let key = Keys::encode('f');
+  let file = read_to_string("input/18.txt").unwrap();
+   let maze = string_to_chargrid(&file);    // println!("{}", &subgrids[1]);
+    let sol1: usize = solution1(&maze);
+    let sol2: usize = solution2(&maze);
+    (Solution::from(sol1), Solution::from(sol2))
+}
+
+#[test]
+fn key_test()
+{  let key = Keys::encode('f');
     dbg!(
-        key.can_open(&Doors::encode('F')),
-        key.can_open(&Doors::encode('Q')),
+        assert!(key.can_open(&Doors::encode('F'))),
+        assert!(!key.can_open(&Doors::encode('Q'))),
     );
     let merged_key = key.add('q');
 
     dbg!(
-        merged_key.can_open(&Doors::encode('F')),
-        merged_key.can_open(&Doors::encode('Q')),
-        merged_key.can_open(&Doors::encode('A')),
+        assert!(merged_key.can_open(&Doors::encode('F'))),
+        assert!(merged_key.can_open(&Doors::encode('Q'))),
+        assert!(!merged_key.can_open(&Doors::encode('A'))),
     );
-    dbg!(
-        merged_key.contains('f'),
-        merged_key.contains('a'),
-        merged_key.contains('q')
-    );
-    let file = read_to_string("input/18.txt").unwrap();
-    let grid = string_to_chargrid(&file);
-    dbg!(grid.width(), grid.height());
-    print!("{grid}");
-    // dbg!(key_paths(&grid, (0,0), 'a'));
-    dbg!(grid.find('@'));
-    let init_pos = grid.find('@').unwrap();
-    dbg!(key_paths(&grid, init_pos, '@'));
-    dbg!(shortest_path(&grid));
-    let subgrids = split_maze(&grid);
-    let dists: usize = subgrids.iter().map(|sg| shortest_path(sg)).sum();
-    dbg!(dists);
-    // println!("{}", &subgrids[1]);
-    (Solution::from(sol1), Solution::from(sol2))
 }
